69a6f86ac688f9318326ae67909caa9316e9b66b
kraulain <kraulain@gmail.com>
2019-09-23 23:34:31 +0100

    grails generate-controller for auth classes

---
+++ grails-app/services/groovyschool/auth/RoleService.groovy
@@ -0,0 +1,18 @@
+package groovyschool.auth
+
+import grails.gorm.services.Service
+
+@Service(Role)
+interface RoleService {
+
+    Role get(Serializable id)
+
+    List<Role> list(Map args)
+
+    Long count()
+
+    void delete(Serializable id)
+
+    Role save(Role role)
+
+}
